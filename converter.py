a3f6aa536d56bfda5ce7cc0bfd746ff382a6ad19
user6174 <edoardo.finauri@gmail.com>
2018-06-17 20:08:50 +0200

    Update converter.py
    Separated card and tokens, added related cards

--- converter.py
+++ converter.py
@@ -6,44 +6,62 @@ tablerow = {"Follower":2,"Spell":3,"Amulet":1}
 #2 → creatures (this includes all cards that have the "creature" type like "Enchantment Creature")
 #3 → non-permanent cards (like Instants and Sorceries)
 
-f = open("/home/sv_database.json")
-g = open("/home/sv_database.xml",'w')
+f = open("/home/makra/SV_COCKATRICE/sv_database.json")
+g = open("/home/makra/SV_COCKATRICE/sv_database.xml",'w')
+h = open("/home/makra/SV_COCKATRICE/sv_tokens.xml",'w')
 g.write( '<cockatrice_carddatabase version="3">')
 g.write( '<cards>')
+h.write( '<cockatrice_carddatabase version="3">')
+h.write( '<cards>')
 
 data = json.load(f)
 for i in data:
-  g.write( '<card>')
-  g.write( '<name>' + str(data[i]["name"]) + '</name>')
-  g.write( '<set>' + str(data[i]["expansion"]) + '</set>')
-  g.write( '<set picURL="https://shadowverse-portal.com/image/card/en/C_' + str(data[i]["id"]) + '.png"></set>')
-  g.write( '<related>' + str(data[i]["name"]) + ' EVOLVED</related>')
-  g.write( '<color>' + str(data[i]["faction"]) + '</color>')
-  g.write( '<manacost>'+str(data[i]["manaCost"])+'</manacost>')
-  g.write( '<cmc>'+str(data[i]["manaCost"])+'</cmc>')
-  g.write( '<type>' + str(data[i]["race"]) + '</type>') 
-  g.write( '<pt>' + str(data[i]["baseData"]["attack"]) + "/" + str(data[i]["baseData"]["defense"]) + '</pt>')
-  g.write( '<tablerow>' + str(tablerow[data[i]["type"]]) + '</tablerow>')
-  g.write( '<text>' + str(data[i]["baseData"]["description"].replace('<br>',' ')) + '</text>')
-  g.write( '<token></token>')
-  g.write( '</card>')
-  g.write( '<card>')
-  g.write( '<name>' + str(data[i]["name"]) + ' EVOLVED</name>')
-  g.write( '<set picURL="https://shadowverse-portal.com/image/card/en/E_' + str(data[i]["id"]) + '.png"></set>')
-  g.write( '<color>' + str(data[i]["faction"]) + '</color>')
-  g.write( '<manacost>'+str(data[i]["manaCost"])+'</manacost>')
-  g.write( '<cmc>'+str(data[i]["manaCost"])+'</cmc>')
-  g.write( '<type>' + str(data[i]["race"]) + '</type>') 
-  g.write( '<pt>' + str(data[i]["evoData"]["attack"]) + "/" + str(data[i]["evoData"]["defense"]) + '</pt>')
-  g.write( '<tablerow>' + str(tablerow[data[i]["type"]]) + '</tablerow>')
-  g.write( '<text>' + str(data[i]["evoData"]["description"].replace('<br>',' ')) + '</text>')
-  g.write( '<token></token>')
-  g.write( '</card>')
+  out=list()
+  out.append( '<card>')
+  out.append( '<name>' + str(data[i]["name"]) + '</name>')
+  out.append( '<set>' + str(data[i]["expansion"]) + '</set>')
+  out.append( '<set picURL="https://shadowverse-portal.com/image/card/en/C_' + str(data[i]["id"]) + '.png"></set>')
+  if data[i]["type"]=="Follower":
+    out.append( '<related>' + str(data[i]["name"]) + ' EVOLVED</related>')
+  for j in data:
+    if data[j]["name"] in data[i]["baseData"]["description"]:
+      out.append( '<related>' + str(data[j]["name"]) + ' </related>')
+  out.append( '<color>' + str(data[i]["faction"]) + '</color>')
+  out.append( '<manacost>'+str(data[i]["manaCost"])+'</manacost>')
+  out.append( '<cmc>'+str(data[i]["manaCost"])+'</cmc>')
+  out.append( '<type>' + str(data[i]["race"]) + '</type>') 
+  out.append( '<pt>' + str(data[i]["baseData"]["attack"]) + "/" + str(data[i]["baseData"]["defense"]) + '</pt>')
+  out.append( '<tablerow>' + str(tablerow[data[i]["type"]]) + '</tablerow>')
+  out.append( '<text>' + str(data[i]["baseData"]["description"].replace('<br>',' ')) + '</text>')
+  if data[i]["expansion"] == "Token":
+    out.append( '<token>1</token>')
+  out.append( '</card>')
+  if data[i]["type"]=="Follower":
+    out.append( '<card>')
+    out.append( '<name>' + str(data[i]["name"]) + ' EVOLVED</name>')
+    out.append( '<set picURL="https://shadowverse-portal.com/image/card/en/E_' + str(data[i]["id"]) + '.png"></set>')
+    out.append( '<color>' + str(data[i]["faction"]) + '</color>')
+    out.append( '<manacost>'+str(data[i]["manaCost"])+'</manacost>')
+    out.append( '<cmc>'+str(data[i]["manaCost"])+'</cmc>')
+    out.append( '<type>' + str(data[i]["race"]) + '</type>') 
+    out.append( '<pt>' + str(data[i]["evoData"]["attack"]) + "/" + str(data[i]["evoData"]["defense"]) + '</pt>')
+    out.append( '<tablerow>' + str(tablerow[data[i]["type"]]) + '</tablerow>')
+    out.append( '<text>' + str(data[i]["evoData"]["description"].replace('<br>',' ')) + '</text>')
+    if data[i]["expansion"] == "Token":
+      out.append( '<token>1</token>')
+    out.append( '</card>')
+  if data[i]["expansion"]=="Token":
+    for i in range(len(out)):
+      h.write(out[i])
+  else:
+    for i in range(len(out)):
+      g.write(out[i])
 g.write( '</cards>')
 g.write( '</cockatrice_carddatabase>')
+h.write( '</cards>')
+h.write( '</cockatrice_carddatabase>')
 g.close()
-
-
+h.close()
 
 
 
