710c72f980c002a7cb7aec8b3eaa7a8391fa2807
user6174 <edoardo.finauri@gmail.com>
2020-02-24 13:12:31 +0100

    Updated formatting and made compatible with dr4ft

--- converter.py
+++ converter.py
@@ -9,86 +9,100 @@ tablerow = {"Follower": 2, "Spell": 3, "Amulet": 1}
 # 1 → non-creature, non-land permanents (like Planeswalkers, Enchantments, and Artifacts)
 # 2 → creatures (this includes all cards that have the "creature" type like "Enchantment Creature")
 # 3 → non-permanent cards (like Instants and Sorceries)
-g.write('''<?xml version="1.0" encoding="UTF-8"?>\n<cockatrice_carddatabase version="4">\n<cards>\n\n''')
-h.write('''<?xml version="1.0" encoding="UTF-8"?>\n<cockatrice_carddatabase version="4">\n<cards>\n\n''')
+sets = ["Basic", "Standard Card Pack", "Darkness Evolved", "Rage of Bahamut", "Tempest of the Gods",
+        "Wonderland Dreams", "Starforged Legends", "Chronogenesis", "Dawnbreak Nightedge",
+        "Brigade of the Sky","Omen of the Ten", "Altersphere", "Steel Rebellion", "Rebirth of Glory",
+        "Verdant Conflict", "Ultimate Colosseum"]
+rarity = {"Bronze": "common",
+          "Silver" : "uncommon",
+          "Gold" : "rare",
+          "Legendary" : "mythic"}
+g.write('''<?xml version="1.0" encoding="UTF-8"?>\n<cockatrice_carddatabase version="4">\n\t<sets>\n''')
+h.write('''<?xml version="1.0" encoding="UTF-8"?>\n<cockatrice_carddatabase version="4">\n\t<cards>\n''')
 odin_string = "<<{me.deck_self.count}+1 ??<br>(Times evolved: <<{me.evolved_card_list.count}>>)>><br>"
+for i in sets:
+    g.write("\t\t<set>\n")
+    g.write("\t\t\t<name>{}</name>\n".format(i))
+    g.write("\t\t\t<longname>{}</longname>\n".format(i))
+    g.write("\t\t\t<settype>Custom</settype>\n")
+    g.write("\t\t</set>\n")
+g.write("\t</sets>\t<cards>\n")
 for i in data:
-    istoken = False
+    token = False
     out = []
-    out.append('<card>\n')
+    out.append('\t\t<card>\n')
     out.append(
-        '    <name>{}</name>\n'.format(str(data[i]["name"].replace('&', 'and'))))
-    out.append('    <text>{}</text>\n'.format(str(data[i]["baseData"]["description"].replace(
+        '\t\t\t<name>{}</name>\n'.format(str(data[i]["name"].replace('&', 'and'))))
+    out.append('\t\t\t<text>{}</text>\n'.format(str(data[i]["baseData"]["description"].replace(
         odin_string, '').replace('<br>', '\n').replace('&', 'and'))))
     if data[i]["expansion"] == "Token":
-        out.append('    <token>1</token>')
-        istoken = True
+        out.append('\t\t\t<token>1</token>')
+        token = True
     out.append(
-        '    <tablerow>{}</tablerow>\n'.format(str(tablerow[data[i]["type"]])))
-    out.append('    <set rarity="{}" picurl="https://sv.bagoum.com/cardF/en/c/{}">{}</set>\n'.format(
-        str(data[i]["rarity"]), str(data[i]["id"]), str(data[i]["expansion"])))
+        '\t\t\t<tablerow>{}</tablerow>\n'.format(str(tablerow[data[i]["type"]])))
+    out.append('\t\t\t<set rarity="{}" picurl="https://sv.bagoum.com/cardF/en/c/{}">{}</set>\n'.format(
+        rarity[str(data[i]["rarity"])], str(data[i]["id"]), str(data[i]["expansion"])))
     for j in data:
         if data[j]["name"][:-1] in data[i]["baseData"]["description"]:
             out.append(
-                '   <related>{}</related>\n'.format(str(data[j]["name"].replace('&', 'and'))))
+                '\t\t\t<related>{}</related>\n'.format(str(data[j]["name"].replace('&', 'and'))))
     if data[i]["type"] == "Follower":
-        out.append('    <related>{} Evolved</related>\n'.format(
+        out.append('\t\t\t<related>{} Evolved</related>\n'.format(
             str(data[i]["name"].replace('&', 'and'))))
     # trait, like machina
     out.append(
-        '    <prop>\n        <type>{}</type>\n'.format(str(data[i]["race"])))
+        '\t\t\t<prop>\n\t\t\t\t<type>{}</type>\n'.format(str(data[i]["race"])))
     # type, like follower
     out.append(
-        '        <maintype>{}</maintype>\n'.format(str(data[i]["type"])))
+        '\t\t\t\t<maintype>{}</maintype>\n'.format(str(data[i]["type"])))
     out.append(
-        '        <manacost>{}</manacost>\n'.format(str(data[i]["manaCost"])))
-    out.append('        <cmc>{}</cmc>\n'.format(str(data[i]["manaCost"])))
-    out.append('        <colors>{}</colors>\n'.format(str(data[i]["faction"])))
+        '\t\t\t\t<manacost>{}</manacost>\n'.format(str(data[i]["manaCost"])))
+    out.append('\t\t\t\t<cmc>{}</cmc>\n'.format(str(data[i]["manaCost"])))
+    out.append('\t\t\t\t<colors>{}</colors>\n'.format(str(data[i]["faction"])))
     out.append(
-        '        <coloridentity>{}</coloridentity>\n'.format(str(data[i]["faction"])))
-    out.append('        <format-standard>{}</format-standard>\n'.format('legal' *
+        '\t\t\t\t<coloridentity>{}</coloridentity>\n'.format(str(data[i]["faction"])))
+    out.append('\t\t\t\t<format-standard>{}</format-standard>\n'.format('legal' *
                                                                         (str(data[i]["rot"]) == 'Rotation')))
-    out.append('        <pt>{}</pt>\n    </prop>\n'.format(
+    out.append('\t\t\t\t<pt>{}</pt>\n\t\t\t</prop>\n'.format(
         str(data[i]["baseData"]["attack"]) + "/" + str(data[i]["baseData"]["defense"])))
-    out.append('</card>\n\n')
+    out.append('\t\t</card>\n')
 
     if data[i]["type"] == "Follower":
         evo_out = list()
-        evo_out.append('<card>\n')
-        evo_out.append('    <name>{} Evolved</name>\n'.format(
+        evo_out.append('\t\t<card>\n')
+        evo_out.append('\t\t\t<name>{} Evolved</name>\n'.format(
             str(data[i]["name"].replace('&', 'and'))))
-        evo_out.append('    <text>{}</text>\n'.format(str(data[i]["evoData"]["description"].replace(
+        evo_out.append('\t\t\t<text>{}</text>\n'.format(str(data[i]["evoData"]["description"].replace(
             odin_string, '').replace('<br>', '\n').replace('&', 'and'))))
-        evo_out.append('    <token>1</token>')
+        evo_out.append('\t\t\t<token>1</token>')
         evo_out.append(
-            '   <tablerow>{}</tablerow>\n'.format(str(tablerow[data[i]["type"]])))
-        evo_out.append('    <set rarity="{}" picurl="https://sv.bagoum.com/cardF/en/e/{}">Token</set>\n'.format(
-            str(data[i]["rarity"]), str(data[i]["id"])))
+            '\t\t\t<tablerow>{}</tablerow>\n'.format(str(tablerow[data[i]["type"]])))
+        evo_out.append('\t\t\t<set rarity="{}" picurl="https://sv.bagoum.com/cardF/en/e/{}">Token</set>\n'.format(
+            rarity[str(data[i]["rarity"])], str(data[i]["id"])))
         for j in data:
             if data[j]["name"][:-1] in data[i]["evoData"]["description"]:
                 evo_out.append(
-                    '   <related>{}</related>\n'.format(str(data[j]["name"].replace('&', 'and'))))
+                    '\t\t\t<related>{}</related>\n'.format(str(data[j]["name"].replace('&', 'and'))))
         evo_out.append(
-            '   <prop>\n        <type>{}</type>\n'.format(str(data[i]["race"])))
+            '\t\t\t<prop>\n\t\t\t\t<type>{}</type>\n'.format(str(data[i]["race"])))
         evo_out.append(
-            '       <maintype>{}</maintype>\n'.format(str(data[i]["type"])))
+            '\t\t\t\t<maintype>{}</maintype>\n'.format(str(data[i]["type"])))
         evo_out.append(
-            '        <cmc>{}</cmc>\n'.format(str(data[i]["manaCost"])))
+            '\t\t\t\t<cmc>{}</cmc>\n'.format(str(data[i]["manaCost"])))
         evo_out.append(
-            '        <colors>{}</colors>\n'.format(str(data[i]["faction"])))
-        evo_out.append('        <pt>{}</pt>\n    </prop>\n'.format(
+            '\t\t\t\t<colors>{}</colors>\n'.format(str(data[i]["faction"])))
+        evo_out.append('\t\t\t\t<pt>{}</pt>\n\t\t\t</prop>\n'.format(
             str(data[i]["evoData"]["attack"]) + "/" + str(data[i]["evoData"]["defense"])))
-        evo_out.append('</card>\n\n')
+        evo_out.append('\t\t</card>\n')
         for i in range(len(evo_out)):
             h.write(evo_out[i])
-    if istoken:
+    if token:
         for i in range(len(out)):
             h.write(out[i])
     else:
         for i in range(len(out)):
             g.write(out[i])
-
-g.write('</cards>\n</cockatrice_carddatabase>')
-h.write('</cards>\n</cockatrice_carddatabase>')
+g.write('\t\t</cards>\n</cockatrice_carddatabase>')
+h.write('\t\t</cards>\n</cockatrice_carddatabase>')
 g.close()
 h.close()
